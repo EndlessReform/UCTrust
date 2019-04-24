b358026c4d83d61f2376f8cd2d9c117e3247f6d7
EndlessReform <jlowellkeisling@gmail.com>
2019-04-24 18:05:51 -0500

    Dynamically generate issues

--- data/issues.js
+++ data/issues.js
@@ -2,19 +2,19 @@ var issues = {
 	sustainability: {
 		name: "Sustainability",
 		byline: "Lowering our footprint", 
-		image: false,
+		image: "http://www.uctrust.us/assets/sustainability.jpg",
 		text: "We need to increase the number and magnitude of sustainability initiatives on campus. We propose a fossil fuel divestment campaign and initiatives to reduce single-use plastics, as well as a move towards composting whenever possible. The current recycling program should be audited and a program informing students about Chicago’s recycling policies should be implemented. We also suggest a SG-organized thrift shop, reducing clothing waste while benefiting students. All new construction should be LEED certified — the new dorm is currently not on track to be. The university administration needs to support these initiatives, at least to the point that we are on par with other schools with similar resources."
 	},
 	health: {
 		name: "Health",
 		byline: "More funds, less wait",
-		image: false,
+		image: "http://www.uctrust.us/assets/health-min.jpg",
 		text: "Students should not be waiting over 6 weeks for an appointment at Student Health Services or to meet a therapist at Student Counseling Services, but this situation is all too common. Timely health services should be a priority for the administration, and wait time figures should published online in the name of transparency — these numbers are already tracked internally. Health services are understaffed and underfunded, and despite the best efforts of the employees, deficiencies are significant. While the new Wellness Center is a step in the right direction, SG needs to make sure it works as intended."
 	},
 	studentUnity: {
 		name: "Student Unity",
 		byline: "Standing together",
-		image: false,
+		image: "http://www.uctrust.us/assets/gsu-min.jpg",
 		text: "We support GSU, and we know that many of you do as well. But right now, it is difficult to direct this energy towards meaningful change. When grad students and undergrads work together, it’s a win-win — we undergrads will have fewer cancelled classes, and grad students will benefit from our support as fellow students who can directly pressure the administration. Student government can and should take the lead in supporting graduate-undergraduate unity. Additionally, we need increased student engagement with SG to hold the administration accountable for student interests. Students should be aware of SG activities, through posts in class Facebook pages, completion and regular maintenance of the SG website, and other avenues. A vocal, aware, and unified student body can push for more projects to improve student well-being. "
 	}
 };
--- js/render.js
+++ js/render.js
@@ -1,33 +1,58 @@
+var util = {
+	newElement: function(type, className, value) {
+		var el = document.createElement(type);
+		el.className = className;
+		el.textContent = value;
+		return el;
+	},
+	newImg: function(className, url) {
+		var img = document.createElement("img");
+		img.className = className;
+		img.src = url;
+		return img;
+	},
+	newLine: function(className) {
+		var line = document.createElement("hr");
+		line.className = className;
+		return line;
+	}
+};
+
 var app = {
 	show: function(obj) {
 		for (var key in obj) {
 			console.log(obj[key].text);
 		};
 	},
-	createIssue: function() {
-		var header = document.createElement("h1");
-		header.textContent = issues[key].name;
-		header.className = "is-card__header";
-
-			var byline = document.createElement("h2");
-			byline.textContent = issues[key].byline;
-			byline.className = "is-card__byline";
-
-			var text = document.createElement("p");
-			text.textContent = issues[key].text;
-			text.className = "is-card__text";
-
-			var wrapper = document.createElement("div");
-			wrapper.className = "is-card";
-
-			wrapper.appendChild(
-
-			return wrapper;
-		};
+	makeIssue: function(issue) {
+		var children = [];
+		children.push(util.newElement("h1","is-card__header", issue.name));
+		children.push(util.newLine("is-card__divider"));
+		children.push(util.newElement("h2","is-card__byline", issue.byline));
+		children.push(util.newLine("is-card__divider"));
+		children.push(util.newImg("is-card__image", issue.image));
+		children.push(util.newElement("p","is-card__text", issue.text));
+		
+		var wrapper = document.createElement("div");
+		wrapper.className = "is-card";
+		children.forEach(function(child) {
+			wrapper.appendChild(child);
+		});
+		
+		return wrapper;
 	},
 	renderIssues: function() {
-		
-	};
+		var newIssues = [];
+		Object.keys(issues).forEach(function(key) {
+			newIssues.push(app.makeIssue(issues[key]));
+		});
+		var target = document.getElementsByClassName("is-cardBox")[0];
+		newIssues.forEach(function(child) {
+			target.appendChild(child);
+		});
+	}
+};
 
-var foo = "bar";
-app.show(issues);
+window.onload = function() {
+	app.renderIssues();
+};
